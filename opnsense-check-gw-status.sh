892c032ea85ea4ed58c409109e0d84d0baccada0
Florian Hild <florian.hild@gmail.com>
2024-06-27 10:37:41 +0200

    add: check_gw_status.sh v2.0 rename to opnsense-check-gw-status.sh :rocket:

--- opnsense-check-gw-status.sh
+++ opnsense-check-gw-status.sh
@@ -7,20 +7,30 @@
 #-------------------------------------------------------------------------------
 
 export LANG=C
-declare -r __SCRIPT_VERSION__='1.0'
+declare -r __SCRIPT_VERSION__='2.0'
 declare -r BASH_LIB_DIR="/usr/local/bin/bash-lib"
 declare -r pluginctl_cmd='/usr/local/sbin/pluginctl'
+declare -r logfile_path="${HOME}/local/log"
 
 # Load libraries
 source ${BASH_LIB_DIR}/logger/lib
 declare -r log_no_timestamp="true"
+
+if ! command -v jq &> /dev/null; then
+  echo "Command jq could not be found"
+  exit 1
+fi
+
+if [[ ! -d ${logfile_path} ]]; then
+  mkdir -p ${logfile_path}
+fi
 
 if ! ping -q -c4 -i1.0 -W40 9.9.9.9 > /dev/null; then
   log "error" "Ping check to \"9.9.9.9\" unsuccessful"
   ${pluginctl_cmd} -r return_gateways_status | jq -c '.dpinger.[]' | while read gw; do
     log "info" "Check Gateway status for \"$(echo "${gw}" | jq -r '.name')\""
     log "info" "Gateway status: \"$(echo "${gw}" | jq -r '.status')\""
-    echo "${gw}" |sed "s/{/{\"timestamp\":\"$(date +'%F %T')\",/" | jq -c >> check_gw_$(echo "${gw}" | jq -r '.name').jsonl
+    echo "${gw}" |sed "s/{/{\"timestamp\":\"$(date +'%F %T')\",/" | jq -c >> ${logfile_path}/check_gw_$(echo "${gw}" | jq -r '.name').jsonl
   done
 else
   log "info" "Ping check to \"9.9.9.9\" successful"
